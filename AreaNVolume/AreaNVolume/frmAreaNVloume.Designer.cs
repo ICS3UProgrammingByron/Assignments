65d599085b071fc7ad1be22ba4eeffc4cc4bd12a
Byron Edmonds Decosse <60893158+ByronED@users.noreply.github.com>
2020-03-26 14:10:44 -0400

    AreaNVolume

--- AreaNVolume/AreaNVolume/frmAreaNVloume.Designer.cs
+++ AreaNVolume/AreaNVolume/frmAreaNVloume.Designer.cs
@@ -28,13 +28,303 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblAreaQ = new System.Windows.Forms.Label();
+            this.btnCalculate1 = new System.Windows.Forms.Button();
+            this.txtBottomBase = new System.Windows.Forms.TextBox();
+            this.txtTopBase = new System.Windows.Forms.TextBox();
+            this.lblVolumeQ1 = new System.Windows.Forms.Label();
+            this.txtHeight2 = new System.Windows.Forms.TextBox();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.btnCalculate2 = new System.Windows.Forms.Button();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.lblBottomBase = new System.Windows.Forms.Label();
+            this.lblVolumeAnswer1 = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblAreaAnswer = new System.Windows.Forms.Label();
+            this.lblTopBase = new System.Windows.Forms.Label();
+            this.lblVolumeQ2 = new System.Windows.Forms.Label();
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.lblHeight2 = new System.Windows.Forms.Label();
+            this.lblVolumeAnswer2 = new System.Windows.Forms.Label();
+            this.txtRadius2 = new System.Windows.Forms.TextBox();
+            this.lblRadius2 = new System.Windows.Forms.Label();
+            this.btnCalculate3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblAreaQ
+            // 
+            this.lblAreaQ.AutoSize = true;
+            this.lblAreaQ.Location = new System.Drawing.Point(376, 11);
+            this.lblAreaQ.Name = "lblAreaQ";
+            this.lblAreaQ.Size = new System.Drawing.Size(312, 13);
+            this.lblAreaQ.TabIndex = 0;
+            this.lblAreaQ.Text = "enter the top and bottom base to calculate the area of a trapizoid";
+            this.lblAreaQ.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnCalculate1
+            // 
+            this.btnCalculate1.Location = new System.Drawing.Point(545, 116);
+            this.btnCalculate1.Name = "btnCalculate1";
+            this.btnCalculate1.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate1.TabIndex = 1;
+            this.btnCalculate1.Text = "Calculate";
+            this.btnCalculate1.UseVisualStyleBackColor = true;
+            this.btnCalculate1.Click += new System.EventHandler(this.btnCalculate1_Click);
+            // 
+            // txtBottomBase
+            // 
+            this.txtBottomBase.Location = new System.Drawing.Point(379, 56);
+            this.txtBottomBase.Name = "txtBottomBase";
+            this.txtBottomBase.Size = new System.Drawing.Size(100, 20);
+            this.txtBottomBase.TabIndex = 2;
+            // 
+            // txtTopBase
+            // 
+            this.txtTopBase.Location = new System.Drawing.Point(545, 56);
+            this.txtTopBase.Name = "txtTopBase";
+            this.txtTopBase.Size = new System.Drawing.Size(100, 20);
+            this.txtTopBase.TabIndex = 3;
+            // 
+            // lblVolumeQ1
+            // 
+            this.lblVolumeQ1.AutoSize = true;
+            this.lblVolumeQ1.Location = new System.Drawing.Point(2, 123);
+            this.lblVolumeQ1.Name = "lblVolumeQ1";
+            this.lblVolumeQ1.Size = new System.Drawing.Size(312, 13);
+            this.lblVolumeQ1.TabIndex = 4;
+            this.lblVolumeQ1.Text = "enter the top and bottom base to calculate the area of a trapizoid";
+            // 
+            // txtHeight2
+            // 
+            this.txtHeight2.Location = new System.Drawing.Point(135, 168);
+            this.txtHeight2.Name = "txtHeight2";
+            this.txtHeight2.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight2.TabIndex = 5;
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(11, 168);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 6;
+            // 
+            // btnCalculate2
+            // 
+            this.btnCalculate2.Location = new System.Drawing.Point(15, 209);
+            this.btnCalculate2.Name = "btnCalculate2";
+            this.btnCalculate2.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate2.TabIndex = 7;
+            this.btnCalculate2.Text = "Calculate";
+            this.btnCalculate2.UseVisualStyleBackColor = true;
+            this.btnCalculate2.Click += new System.EventHandler(this.btnCalculate2_Click);
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(379, 116);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 8;
+            // 
+            // lblBottomBase
+            // 
+            this.lblBottomBase.AutoSize = true;
+            this.lblBottomBase.Location = new System.Drawing.Point(387, 40);
+            this.lblBottomBase.Name = "lblBottomBase";
+            this.lblBottomBase.Size = new System.Drawing.Size(79, 13);
+            this.lblBottomBase.TabIndex = 9;
+            this.lblBottomBase.Text = "Bottom Base(b)";
+            // 
+            // lblVolumeAnswer1
+            // 
+            this.lblVolumeAnswer1.AutoSize = true;
+            this.lblVolumeAnswer1.Location = new System.Drawing.Point(169, 219);
+            this.lblVolumeAnswer1.Name = "lblVolumeAnswer1";
+            this.lblVolumeAnswer1.Size = new System.Drawing.Size(25, 13);
+            this.lblVolumeAnswer1.TabIndex = 10;
+            this.lblVolumeAnswer1.Text = "???";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(413, 90);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(38, 13);
+            this.lblHeight.TabIndex = 11;
+            this.lblHeight.Text = "Height";
+            // 
+            // lblAreaAnswer
+            // 
+            this.lblAreaAnswer.AutoSize = true;
+            this.lblAreaAnswer.Location = new System.Drawing.Point(678, 90);
+            this.lblAreaAnswer.Name = "lblAreaAnswer";
+            this.lblAreaAnswer.Size = new System.Drawing.Size(25, 13);
+            this.lblAreaAnswer.TabIndex = 12;
+            this.lblAreaAnswer.Text = "???";
+            // 
+            // lblTopBase
+            // 
+            this.lblTopBase.AutoSize = true;
+            this.lblTopBase.Location = new System.Drawing.Point(569, 40);
+            this.lblTopBase.Name = "lblTopBase";
+            this.lblTopBase.Size = new System.Drawing.Size(65, 13);
+            this.lblTopBase.TabIndex = 13;
+            this.lblTopBase.Text = "Top Base(a)";
+            // 
+            // lblVolumeQ2
+            // 
+            this.lblVolumeQ2.AutoSize = true;
+            this.lblVolumeQ2.Location = new System.Drawing.Point(382, 267);
+            this.lblVolumeQ2.Name = "lblVolumeQ2";
+            this.lblVolumeQ2.Size = new System.Drawing.Size(252, 13);
+            this.lblVolumeQ2.TabIndex = 14;
+            this.lblVolumeQ2.Text = "enter the radius to calculate the volume of a sphere ";
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Location = new System.Drawing.Point(40, 152);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(40, 13);
+            this.lblRadius.TabIndex = 15;
+            this.lblRadius.Text = "Radius";
+            // 
+            // lblHeight2
+            // 
+            this.lblHeight2.AutoSize = true;
+            this.lblHeight2.Location = new System.Drawing.Point(169, 152);
+            this.lblHeight2.Name = "lblHeight2";
+            this.lblHeight2.Size = new System.Drawing.Size(38, 13);
+            this.lblHeight2.TabIndex = 16;
+            this.lblHeight2.Text = "Height";
+            // 
+            // lblVolumeAnswer2
+            // 
+            this.lblVolumeAnswer2.AutoSize = true;
+            this.lblVolumeAnswer2.Location = new System.Drawing.Point(528, 328);
+            this.lblVolumeAnswer2.Name = "lblVolumeAnswer2";
+            this.lblVolumeAnswer2.Size = new System.Drawing.Size(25, 13);
+            this.lblVolumeAnswer2.TabIndex = 17;
+            this.lblVolumeAnswer2.Text = "???";
+            // 
+            // txtRadius2
+            // 
+            this.txtRadius2.Location = new System.Drawing.Point(379, 321);
+            this.txtRadius2.Name = "txtRadius2";
+            this.txtRadius2.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius2.TabIndex = 18;
+            // 
+            // lblRadius2
+            // 
+            this.lblRadius2.AutoSize = true;
+            this.lblRadius2.Location = new System.Drawing.Point(411, 295);
+            this.lblRadius2.Name = "lblRadius2";
+            this.lblRadius2.Size = new System.Drawing.Size(40, 13);
+            this.lblRadius2.TabIndex = 19;
+            this.lblRadius2.Text = "Radius";
+            // 
+            // btnCalculate3
+            // 
+            this.btnCalculate3.Location = new System.Drawing.Point(390, 361);
+            this.btnCalculate3.Name = "btnCalculate3";
+            this.btnCalculate3.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate3.TabIndex = 20;
+            this.btnCalculate3.Text = "Calculate";
+            this.btnCalculate3.UseVisualStyleBackColor = true;
+            this.btnCalculate3.Click += new System.EventHandler(this.btnCalculate3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 21;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(180, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // frmAreaNVloume
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnCalculate3);
+            this.Controls.Add(this.lblRadius2);
+            this.Controls.Add(this.txtRadius2);
+            this.Controls.Add(this.lblVolumeAnswer2);
+            this.Controls.Add(this.lblHeight2);
+            this.Controls.Add(this.lblRadius);
+            this.Controls.Add(this.lblVolumeQ2);
+            this.Controls.Add(this.lblTopBase);
+            this.Controls.Add(this.lblAreaAnswer);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblVolumeAnswer1);
+            this.Controls.Add(this.lblBottomBase);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.btnCalculate2);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.txtHeight2);
+            this.Controls.Add(this.lblVolumeQ1);
+            this.Controls.Add(this.txtTopBase);
+            this.Controls.Add(this.txtBottomBase);
+            this.Controls.Add(this.btnCalculate1);
+            this.Controls.Add(this.lblAreaQ);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmAreaNVloume";
+            this.Text = "Area And Volume By Byron";
+            this.Load += new System.EventHandler(this.frmAreaNVloume_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblAreaQ;
+        private System.Windows.Forms.Button btnCalculate1;
+        private System.Windows.Forms.TextBox txtBottomBase;
+        private System.Windows.Forms.TextBox txtTopBase;
+        private System.Windows.Forms.Label lblVolumeQ1;
+        private System.Windows.Forms.TextBox txtHeight2;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Button btnCalculate2;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label lblBottomBase;
+        private System.Windows.Forms.Label lblVolumeAnswer1;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblAreaAnswer;
+        private System.Windows.Forms.Label lblTopBase;
+        private System.Windows.Forms.Label lblVolumeQ2;
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.Label lblHeight2;
+        private System.Windows.Forms.Label lblVolumeAnswer2;
+        private System.Windows.Forms.TextBox txtRadius2;
+        private System.Windows.Forms.Label lblRadius2;
+        private System.Windows.Forms.Button btnCalculate3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
     }
 }
 
--- AreaNVolume/AreaNVolume/frmAreaNVloume.cs
+++ AreaNVolume/AreaNVolume/frmAreaNVloume.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Byron Edmonds-Decosse
+ * Created on: 26-3-2020
+ * Created for: ICS3U Programming
+ * Assignment #4b - AreaNVolume
+ * This program calculates the area and volume of various shapes when the user gives the measurements and clicks on the calculate button
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +22,88 @@ namespace AreaNVolume
         public frmAreaNVloume()
         {
             InitializeComponent();
+        }
+
+        private void frmAreaNVloume_Load(object sender, EventArgs e)
+        {
+            //hide the labels 
+            this.lblVolumeAnswer2.Hide();
+
+            this.lblVolumeAnswer1.Hide();
+
+            this.lblAreaAnswer.Hide();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate1_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            double topbase, bottombase, height, area;
+
+            //convert to string from each text box to a double
+            topbase = double.Parse(txtTopBase.Text);
+
+            bottombase = double.Parse(txtBottomBase.Text);
+
+            height = double.Parse(txtHeight.Text);
+
+            //calculate the area
+
+            area = 1 / 2 * (topbase * bottombase) * height;
+
+            //insert the area into their respective label
+            this.lblAreaAnswer.Text = Convert.ToString(area) + " squared centimeters";
+
+            //display the area label with their respective answers
+            this.lblAreaAnswer.Show();
+        }
+
+        private void btnCalculate2_Click(object sender, EventArgs e)
+        {
+            //declare local variables 
+            double height, radius, volume;
+
+            //convert the string from each text box to a double
+            radius = double.Parse(txtRadius.Text);
+
+            height = double.Parse(txtHeight2.Text);
+
+            //calculate the volume of the cylinder
+            volume = Math.PI * Math.Pow(radius, 2) * height;
+
+            //insert the volume into their respective label
+            this.lblVolumeAnswer1.Text = Convert.ToString(volume) + " cubed centimeters";
+
+            //display the volume label with the respective answer
+            this.lblVolumeAnswer1.Show();
+        }
+
+        private void btnCalculate3_Click(object sender, EventArgs e)
+        {
+            //declare local variables 
+            double radius, volume;
+
+            //convert the string from each text box to a double 
+            radius = double.Parse(txtRadius2.Text);
+
+            //calculate the the volume of a sphere
+            volume = 4 / 3 * Math.PI * Math.Pow(radius, 3);
+
+            //insert the volume into their respective label
+            this.lblVolumeAnswer2.Text = Convert.ToString(volume) + " cubed centimeters";
+
+            //display the volume with the respective ansewr
+            this.lblVolumeAnswer2.Show();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            //close the program
+            this.Close();
         }
     }
 }
